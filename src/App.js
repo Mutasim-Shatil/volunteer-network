4e906e1aa111b0fe6b21114a10badece80963079
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-03-16 23:55:10 +0800

    set firebase google login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from './components/Home/Home';
+import "./App.css";
+// import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import { createContext, useState } from "react";
+import NoMatch from "./components/NoMatch";
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <Router>
-      {/* <Navbar /> */}
-      <Switch>
-        <Route>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyApnSPE3Vzzn9ADU6WrorAeHd2LO879PqY",
+  authDomain: "volunteer-network-af428.firebaseapp.com",
+  projectId: "volunteer-network-af428",
+  storageBucket: "volunteer-network-af428.appspot.com",
+  messagingSenderId: "914379500516",
+  appId: "1:914379500516:web:80952e47d91136e19ef5f1",
+};
+export default firebaseConfig;
