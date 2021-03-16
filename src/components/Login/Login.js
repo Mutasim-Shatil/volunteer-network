252d7952dd825da184bf7b03c75c377358bf03f6
Mutasim-Shatil <mutasimshatil@hotmail.com>
2021-03-17 01:00:20 +0800

    design login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,9 @@ import "firebase/auth";
 import firebaseConfig from "./firebase.config";
 import { useHistory, useLocation } from "react-router-dom";
 import { UserContext } from "../../App";
+import navbarLogo from "../../logos/navbar-logo.png"
+import './Login.css';
+import google from '../../logos/google.jpg'
 
 const Login = () => {
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -33,9 +36,18 @@ const Login = () => {
       });
   };
   return (
-    <div>
-      <h1>This is Login</h1>
-      <button onClick={handleGoogleSignIn}>Google Sign in</button>
+    <div className="login">
+      <div className="login__header">
+        <img src={navbarLogo} alt="" />
+      </div>
+      <div className="login__section">
+        <h3>Login with</h3>
+        <div className="login__btn">
+          <button onClick={handleGoogleSignIn}>Google Sign in</button>
+        </div>
+      </div>
+      {/* <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign in</button> */}
     </div>
   );
 };
